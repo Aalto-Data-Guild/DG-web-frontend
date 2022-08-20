051236701e0e9c8bdb8717bd236372c165737a47
gydis <egor.eremin@aalto.fi>
2022-08-20 16:25:44 +0300

    Add header to the rest of pages. Fill with lorem.

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,5 +1,18 @@
+import Header from "./Header";
+import Sidecolumn from "./Sidecolumn";
+
 const Events = () => {
-  return <div></div>;
+  const lorem =
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+  const contents = [{ header: "Lorem", text: lorem, id: 1 }];
+  return (
+    <div>
+      <Header />
+      <div className="cont">
+        <Sidecolumn contents={contents} />
+      </div>
+    </div>
+  );
 };
 
 export default Events;
